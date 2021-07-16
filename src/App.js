aea2e3cb89326a8f41e4143d14e621e0c5a9e461
Omar <omaar_5@hotmail.com>
2021-07-16 02:22:29 +0200

    feat: pass headerData

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import { GlobalStyle, theme } from "./GlobalStyle";
 import { ThemeProvider } from 'styled-components';
 import styled from "styled-components/macro";
 import { Header } from "./Components";
+import resumeData from "./data/resumeData.json";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Application>
-        <Header />
+        <Header data={resumeData.header} />
       </Application>
     </ThemeProvider>
   );
