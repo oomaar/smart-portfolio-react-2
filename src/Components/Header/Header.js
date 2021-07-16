744c10f99db83673467f71b88f3c892022731a4a
Omar <omaar_5@hotmail.com>
2021-07-16 02:35:32 +0200

    feat: Header part-1

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,65 @@
+import { useEffect, useState } from "react";
 import {
-    Test,
+    HeaderTag,
+    Nav,
+    Logo,
+    NavMenu,
+    NavList,
+    NavItem,
+    NavLink,
+    NavToggle,
 } from "./styledHeader";
 
-export const Header = () => {
+export const Header = ({ data }) => {
+    const [show, setShow] = useState(false);
+    const [scrollHeader, setScrollHeader] = useState(false);
+
+    const toggleShow = () => setShow(state => !state);
+    const toggleHide = () => setShow(false);
+    const showShadow = () => window.scrollY > 100 ? setScrollHeader(true) : setScrollHeader(false);
+
+    // Icons Declaration
+    const bxMenu = 'bx-menu';
+    const bxX = 'bx-x';
+
+    const links = data.map(link => (
+        <NavItem key={link.id}>
+            <NavLink
+                scrollHeader={scrollHeader}
+                href={link.href}
+                to={link.href}
+                smooth={true}
+                duration={50}
+                spy={true}
+                exact="true"
+                offset={-50}
+                activeClass="active-link"
+                onClick={toggleHide}
+            >
+                {link.name}
+            </NavLink>
+        </NavItem>
+    ));
+
+    useEffect(() => {
+        window.addEventListener("scroll", showShadow);
+    }, []);
+
     return (
-        <Test>
-            Header Component
-        </Test>
+        <HeaderTag scrollHeader={scrollHeader}>
+            <Nav>
+                <Logo scrollHeader={scrollHeader}>Omar Hassan</Logo>
+
+                <NavMenu scrollHeader={scrollHeader} show={show}>
+                    <NavList>
+                        {links}
+                    </NavList>
+                </NavMenu>
+
+                <NavToggle scrollHeader={scrollHeader} onClick={toggleShow}>
+                    <i className={`bx ${show ? bxX : bxMenu}`}></i>
+                </NavToggle>
+            </Nav>
+        </HeaderTag>
     );
 };
--- src/Components/Header/styledHeader.js
+++ src/Components/Header/styledHeader.js
@@ -1,3 +1,71 @@
 import styled from "styled-components/macro";
+import { Link as ScrollLink } from "react-scroll";
 
-export const Test = styled.div``;
+export const HeaderTag = styled.header`
+  background-color: ${({ theme, scrollHeader }) => scrollHeader ? theme.color.firstColorLighten : '#000'};
+  box-shadow: ${({ scrollHeader }) => scrollHeader && '0 2px 4px rgba(0, 0, 0, 0.1)'};
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: ${({ theme }) => theme.zIndex.zHeader};
+`;
+export const Nav = styled.nav`
+  height: ${({ theme }) => theme.HeaderHeight};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Logo = styled.a`
+  color: ${({ theme, scrollHeader }) => scrollHeader ? theme.color.firstColorDark : theme.color.firstColorLighten};
+`;
+
+export const NavMenu = styled.div`
+  @media screen and (min-width: 768px) {
+      position: fixed;
+      top: ${({ theme, show }) => show ? theme.HeaderHeight : "-100%"};
+      left: 0;
+      width: 100%;
+      padding-top: 1.5rem;
+      text-align: center;
+      background-color: ${({ theme, scrollHeader }) => scrollHeader ? theme.color.firstColorLighten : '#000'};
+      transition: 0.4s;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+      border-radius: 0 0 1rem 1rem;
+  }
+`;
+
+export const NavList = styled.ul`
+  .active-link {
+      position: relative;
+
+      ::after {
+        content: '';
+        position: absolute;
+        bottom: -0.7rem;
+        left: 0;
+        width: 65%;
+        height: 3px;
+        background-color: ${({ theme }) => theme.color.firstColorLight};
+      }
+  }
+`;
+
+export const NavItem = styled.li`
+  margin-bottom: ${({ theme }) => theme.margin.mb_4};
+`;
+
+export const NavLink = styled(ScrollLink)`
+  color: ${({ theme, scrollHeader }) => scrollHeader ? theme.color.firstColorDark : theme.color.firstColorLighten};
+
+  :hover {
+    color: ${({ theme }) => theme.color.firstColorLight};
+  }
+`;
+
+export const NavToggle = styled.div`
+  color: ${({ theme, scrollHeader }) => scrollHeader ? theme.color.firstColorDark : theme.color.firstColorLighten};
+  font-size: 1.3rem;
+  cursor: pointer;
+`;
