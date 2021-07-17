b496f0691008bd606121234dadf442fedc453109
Omar <omaar_5@hotmail.com>
2021-07-17 02:20:57 +0200

    feat: About Part-1

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,11 +1,40 @@
+import { Container, Section, SectionSubtitle, SectionTitle } from "../../GlobalStyle";
 import {
-    Test
+    AboutContainer,
+    AboutData,
+    AboutDescription,
+    AboutNumber,
+    AboutImage,
 } from "./styledAbout";
 
-export const About = () => {
+export const About = ({ data }) => {
+    const description = data.description;
+    const experience = data.experience;
+
     return (
-        <Test>
-            About Component
-        </Test>
+        <Section>
+            <Container>
+                <SectionSubtitle>My history</SectionSubtitle>
+                <SectionTitle>About me</SectionTitle>
+
+                <AboutContainer>
+                    <AboutData>
+                        <AboutDescription>
+                            <span>Hello, I am a <br /></span>
+                            {description}
+                        </AboutDescription>
+
+                        {experience.map((exp, i) => (
+                            <div key={i}>
+                                <AboutNumber>{exp.number}+ </AboutNumber>
+                                <span>{exp.achevement}</span>
+                            </div>
+                        ))}
+
+                        <AboutImage src="/img/about.jpg" />
+                    </AboutData>
+                </AboutContainer>
+            </Container>
+        </Section>
     );
 };
--- src/Components/About/styledAbout.js
+++ src/Components/About/styledAbout.js
@@ -1,3 +1,30 @@
 import styled from 'styled-components/macro';
 
-export const Test = styled.h1``;
+export const AboutContainer = styled.div`
+  display: grid;
+  gap: 1.5rem;
+`;
+
+export const AboutData = styled.div`
+  display: grid;
+  gap: 1.5rem;
+  text-align: center;
+`;
+
+export const AboutDescription = styled.p`
+  font-size: ${({ theme }) => theme.font.h2FontSize};
+  font-weight: ${({ theme }) => theme.font.fontWeight.semiBold};
+  color: ${({ theme }) => theme.color.firstColor};
+  `;
+
+export const AboutNumber = styled.span`
+  display: block;
+  font-size: ${({ theme }) => theme.font.h1FontSize};
+  color: ${({ theme }) => theme.color.firstColor};
+  `;
+
+export const AboutImage = styled.img`
+  justify-self: center;
+  width: 220px;
+  border-radius: 0.5rem;
+`;
