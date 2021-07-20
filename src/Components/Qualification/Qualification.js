4797a877e32671b781f1d693286d22b13b29cac5
Omar <omaar_5@hotmail.com>
2021-07-20 18:26:58 +0200

    feat: Qualification part-1

--- src/Components/Qualification/Qualification.js
+++ src/Components/Qualification/Qualification.js
@@ -1,11 +1,91 @@
+import { Container, Section, SectionSubtitle, SectionTitle } from "../../GlobalStyle";
 import {
-    Test,
+    QualificationContainer,
+    QualificationTitle,
+    QualificationIcon,
+    QualificationGrid,
+    QualificationArea,
+    QualificationBox,
+    QualificationWork,
+    QualificationSmallIcon,
 } from './styledQualification';
 
-export const Qualification = () => {
+export const Qualification = ({ data }) => {
+    const workData = data.work.map(work => (
+        <div key={work.id}>
+            <QualificationArea>{work.title}</QualificationArea>
+            <QualificationBox>
+                <QualificationWork>
+                    <QualificationSmallIcon>
+                        <i className="bx bx-briefcase-alt"></i>
+                    </QualificationSmallIcon>
+                    {work.place}
+                </QualificationWork>
+                <QualificationWork>
+                    <QualificationSmallIcon>
+                        <i className="bx bx-calendar-alt"></i>
+                    </QualificationSmallIcon>
+                    {work.date}
+                </QualificationWork>
+            </QualificationBox>
+        </div>
+    ));
+
+    const educationData = data.education.map(education => (
+        <div key={education.id}>
+            <QualificationArea>{education.title}</QualificationArea>
+
+            <QualificationBox>
+                <QualificationWork>
+                    <QualificationSmallIcon>
+                        <i className="bx bx-book-alt"></i>
+                    </QualificationSmallIcon>
+                    {education.place}
+                </QualificationWork>
+                <QualificationWork>
+                    <QualificationSmallIcon>
+                        <i className="bx bx-calendar-alt"></i>
+                    </QualificationSmallIcon>
+                    {education.date}
+                </QualificationWork>
+            </QualificationBox>
+        </div>
+    ));
+
     return (
-        <Test>
-            Qualification component
-        </Test>
+        <Section>
+            <SectionSubtitle>Experince and education</SectionSubtitle>
+            <SectionTitle>Qualification</SectionTitle>
+
+            <Container>
+                <QualificationContainer>
+                    <div>
+                        <QualificationTitle>
+                            <QualificationIcon>
+                                <i className="bx bx-briefcase-alt"></i>
+                            </QualificationIcon>
+                            Work Experince
+                        </QualificationTitle>
+
+                        <QualificationGrid>
+                            {workData}
+                        </QualificationGrid>
+                    </div>
+
+                    <div>
+                        <QualificationTitle>
+                            <QualificationIcon>
+                                <i className="bx bx-book-bookmark"></i>
+                            </QualificationIcon>
+                            Education
+                        </QualificationTitle>
+
+                        <QualificationGrid>
+                            {educationData}
+                        </QualificationGrid>
+                    </div>
+                </QualificationContainer>
+            </Container>
+        </Section>
     );
 };
--- src/Components/Qualification/styledQualification.js
+++ src/Components/Qualification/styledQualification.js
@@ -1,3 +1,50 @@
 import styled from 'styled-components/macro';
 
-export const Test = styled.div``;
+export const QualificationContainer = styled.div`
+  display: grid;
+  row-gap: 2.5rem;
+`;
+
+export const QualificationTitle = styled.h3`
+  font-size: ${({ theme }) => theme.font.h3fontSize};
+  color: ${({ theme }) => theme.color.firstColor};
+  margin-bottom: ${({ theme }) => theme.margin.mb_2};
+  display: flex;
+  align-items: center;
+`;
+
+export const QualificationIcon = styled.div`
+  font-size: 1.5rem;
+  margin-right: ${({ theme }) => theme.margin.mb_1};
+`;
+
+export const QualificationGrid = styled.div`
+  display: grid;
+  gap: 1.5rem;
+`;
+
+export const QualificationArea = styled.h3`
+  font-size: ${({ theme }) => theme.font.normalFontSize};
+  color: ${({ theme }) => theme.color.firstColor};
+  margin-bottom: ${({ theme }) => theme.margin.mb_1};
+`;
+
+export const QualificationBox = styled.div`
+  padding-bottom: 1rem;
+  border-bottom: 1px solid #ccc;
+  font-weight: ${({ theme }) => theme.font.fontWeight.medium};
+`;
+
+export const QualificationWork = styled.span`
+  display: flex;
+  align-items: center;
+  font-size: ${({ theme }) => theme.font.smallerFontSize};
+  color: ${({ theme }) => theme.color.firstColorLight};
+  padding: 0.3rem 0;
+`;
+
+export const QualificationSmallIcon = styled.div`
+  font-size: ${({ theme }) => theme.font.smallerFontSize};
+  color: ${({ theme }) => theme.color.firstColorLight};
+  margin-right: ${({ theme }) => theme.margin.mb_1};
+`;
