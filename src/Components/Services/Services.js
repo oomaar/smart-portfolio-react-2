1ab87301fdf240b4428e175ac13f904f2109c2da
Omar <omaar_5@hotmail.com>
2021-07-21 13:23:11 +0200

    feat: Services part-1

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,11 +1,33 @@
+import { Container, Section, SectionSubtitle, SectionTitle, Button } from "../../GlobalStyle";
 import {
-    Test,
+    ServicesContainer,
+    ServicesData,
+    ServicesIcon,
+    ServicesTitle,
+    ServicesDescription,
 } from "./styledServices";
 
-export const Services = () => {
+export const Services = ({ data }) => {
+    const servicesData = data.map(service => (
+        <ServicesData key={service.id}>
+            <ServicesIcon>
+                <i className={service.icon}></i>
+            </ServicesIcon>
+            <ServicesTitle>{service.title}</ServicesTitle>
+            <ServicesDescription>{service.description}</ServicesDescription>
+            <Button>Know More</Button>
+        </ServicesData>
+    ));
     return (
-        <Test>
-            Services Component
-        </Test>
+        <Section id="services">
+            <SectionSubtitle>What I offer</SectionSubtitle>
+            <SectionTitle>Services</SectionTitle>
+
+            <Container>
+                <ServicesContainer>
+                    {servicesData}
+                </ServicesContainer>
+            </Container>
+        </Section>
     );
 };
--- src/Components/Services/styledServices.js
+++ src/Components/Services/styledServices.js
@@ -1,3 +1,36 @@
 import styled from 'styled-components/macro';
 
-export const Test = styled.div``;
+export const ServicesContainer = styled.div`
+  display: grid;
+  gap: 1.5rem;
+`;
+
+export const ServicesData = styled.div`
+  padding: 3rem 1rem;
+  background-color: #fff;
+  border-radius: 0.5rem;
+  text-align: center;
+  transition: 0.4s;
+  box-shadow: 0 4px 6px rgba(174, 190, 205, 0.3);
+
+  :hover {
+    transform: translateY(-0.5rem);
+    box-shadow: 0 6px 8px rgba(174, 190, 205, 0.3);
+  }
+`;
+
+export const ServicesIcon = styled.div`
+  margin-bottom: ${({ theme }) => theme.margin.mb_2};
+  color: ${({ theme }) => theme.color.firstColor};
+  font-size: 3rem;
+`;
+
+export const ServicesTitle = styled.h3`
+  margin-bottom: ${({ theme }) => theme.margin.mb_2};
+  color: ${({ theme }) => theme.color.firstColor};
+  font-size: ${({ theme }) => theme.font.h3fontSize};
+`;
+
+export const ServicesDescription = styled.p`
+  margin-bottom: ${({ theme }) => theme.margin.mb_4};
+`;
