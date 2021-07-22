b8d871d3cca2c5e893bcfc4f141e980e2438fd73
Omar <omaar_5@hotmail.com>
2021-07-22 15:16:01 +0200

    feat: Portfolio part-1

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,11 +1,91 @@
+import { useState } from "react";
+import { Button, Container, Section, SectionSubtitle, SectionTitle } from "../../GlobalStyle";
 import {
     PortfolioContainer,
+    PortfolioNav,
+    PortfolioItem,
+    PortfolioContent,
+    Image,
+    PortfolioData,
+    PortfolioSubtitle,
+    Title,
 } from "./styledPortfolio";
 
-export const Portfolio = () => {
+export const Portfolio = ({ data }) => {
+    const [activeData, setActiveData] = useState("all");
+    const handleActiveData = name => setActiveData(name);
+
+    const webDevelopmentData = data.webDevelopment.map(project => (
+        <PortfolioContent key={project.id}>
+            <a href={project.link}>
+                <Image src={project.img} />
+            </a>
+            <PortfolioData>
+                <PortfolioSubtitle>{project.subTitle}</PortfolioSubtitle>
+                <a href={project.link}>
+                    <Title>{project.title}</Title>
+                </a>
+                <Button link>View Details</Button>
+            </PortfolioData>
+        </PortfolioContent>
+    ));
+
+    const uxDesignData = data.uxDesign.map(project => (
+        <PortfolioContent key={project.id}>
+            <a href={project.link}>
+                <Image src={project.img} />
+            </a>
+            <PortfolioData>
+                <PortfolioSubtitle>{project.subTitle}</PortfolioSubtitle>
+                <a href={project.link}>
+                    <Title>{project.title}</Title>
+                </a>
+                <Button link>View Details</Button>
+            </PortfolioData>
+        </PortfolioContent>
+    ));
+
+    const mobileAppData = data.mobileApp.map(project => (
+        <PortfolioContent key={project.id}>
+            <a href={project.link}>
+                <Image src={project.img} />
+            </a>
+            <PortfolioData>
+                <PortfolioSubtitle>{project.subTitle}</PortfolioSubtitle>
+                <a href={project.link}>
+                    <Title>{project.title}</Title>
+                </a>
+                <Button link>View Details</Button>
+            </PortfolioData>
+        </PortfolioContent>
+    ));
+
     return (
-        <PortfolioContainer>
-            Portfolio component
-        </PortfolioContainer>
-    )
+        <Section id="portfolio">
+            <Container>
+                <SectionSubtitle>My recent work</SectionSubtitle>
+                <SectionTitle>Portfolio</SectionTitle>
+
+                <PortfolioNav>
+                    <PortfolioItem onClick={() => handleActiveData("all")}>All</PortfolioItem>
+                    <PortfolioItem onClick={() => handleActiveData("web")}>Web</PortfolioItem>
+                    <PortfolioItem onClick={() => handleActiveData("ux")}>Ui/Ux</PortfolioItem>
+                    <PortfolioItem onClick={() => handleActiveData("app")}>App</PortfolioItem>
+                </PortfolioNav>
+
+                <PortfolioContainer>
+                    {activeData === "all" && (
+                        <>
+                            {webDevelopmentData}
+                            {uxDesignData}
+                            {mobileAppData}
+                        </>
+                    )}
+                    {activeData === "web" && webDevelopmentData}
+                    {activeData === "ux" && uxDesignData}
+                    {activeData === "app" && mobileAppData}
+                </PortfolioContainer>
+            </Container>
+        </Section>
+    );
 };
--- src/Components/Portfolio/styledPortfolio.js
+++ src/Components/Portfolio/styledPortfolio.js
@@ -1,3 +1,62 @@
-import styled from 'styled-components/macro';
+import styled, { keyframes } from 'styled-components/macro';
 
-export const PortfolioContainer = styled.div``;
+const animate = keyframes`
+  from {
+    transform: scale(1);
+  }
+  
+  to {
+    transform: scale(0);
+  }
+`;
+
+export const PortfolioContainer = styled.div`
+  display: grid;
+  gap: 1.5rem;
+`;
+
+export const PortfolioNav = styled.div`
+  text-align: center;
+  margin-bottom: ${({ theme }) => theme.margin.mb_3};
+`;
+
+export const PortfolioItem = styled.span`
+  margin: 0 ${({ theme }) => theme.margin.mb_2};
+  cursor: pointer;
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  transition: 0.4s;
+`;
+
+export const PortfolioContent = styled.div`
+  background-color: #fff;
+  border-radius: 0.5rem;
+  overflow: hidden;
+  box-shadow: 0 4px 6px rgba(174, 190, 205, 0.3);
+  animation: ${animate} 0.1s linear none;
+
+  :hover {
+    box-shadow: 0 6px 8px rgba(174, 190, 205, 0.3);
+  }
+
+  :hover ${Image} {
+    transform: scale(1.02);
+  }
+`;
+
+export const PortfolioData = styled.div`
+  padding:  1.5rem;
+`;
+
+export const PortfolioSubtitle = styled.span`
+  font-size: ${({ theme }) => theme.font.smallFontSize};
+  color: ${({ theme }) => theme.color.firstColorLight};  
+`;
+
+export const Title = styled.h3`
+  font-size: ${({ theme }) => theme.font.h3FontSize};
+  color: ${({ theme }) => theme.color.firstColor};
+  margin: ${({ theme }) => theme.margin.mb_2} 0;
+`;
