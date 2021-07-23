875b36124b63762ac2a0566941ffc61f810dbfde
Omar <omaar_5@hotmail.com>
2021-07-23 14:10:27 +0200

    feat: Testimonial part-1

--- src/Components/Testimonial/Testimonial.js
+++ src/Components/Testimonial/Testimonial.js
@@ -1,11 +1,46 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Pagination } from 'swiper';
+import 'swiper/swiper-bundle.css';
+import { Container, Section, SectionSubtitle, SectionTitle } from "../../GlobalStyle";
 import {
     TestimonialContainer,
+    TestimonialContent,
+    TestimonialImage,
+    TestimonialName,
+    TestimonialClient,
+    TestimonialDescription,
 } from "./styledTestimonial";
 
-export const Testimonial = () => {
+SwiperCore.use([Pagination]);
+
+export const Testimonial = ({ data }) => {
+    const testimonialData = data.map(client => (
+        <SwiperSlide key={client.id}>
+            <TestimonialContent>
+                <TestimonialImage src={client.img} />
+                <TestimonialName>{client.name}</TestimonialName>
+                <TestimonialClient>Client</TestimonialClient>
+                <TestimonialDescription>{client.description}</TestimonialDescription>
+            </TestimonialContent>
+        </SwiperSlide>
+    ));
+
     return (
-        <TestimonialContainer>
-            Testimonial Component
-        </TestimonialContainer>
+        <Section>
+            <Container>
+                <SectionSubtitle>My client saying</SectionSubtitle>
+                <SectionTitle>Testimonial</SectionTitle>
+
+                <TestimonialContainer>
+                    <Swiper
+                        loop='true'
+                        pagination={{ clickable: true }}
+                    >
+                        {testimonialData}
+                    </Swiper>
+                    <div className="swiper-pagination"></div>
+                </TestimonialContainer>
+            </Container>
+        </Section>
     );
 };
--- src/Components/Testimonial/styledTestimonial.js
+++ src/Components/Testimonial/styledTestimonial.js
@@ -1,3 +1,53 @@
 import styled from 'styled-components/macro';
 
-export const TestimonialContainer = styled.div``;
+export const TestimonialContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => theme.color.firstColor};
+  color: ${({ theme }) => theme.color.firstColorLighten};
+  border-radius: 0.5rem;
+  padding: 2rem 1rem;
+  box-shadow: 0 4px 8px rgba(47, 10, 13, 0.25);
+  text-align: center;
+
+  .swiper-pagination {
+    bottom: 0;
+  }
+
+  .swiper-pagination-bullet {
+    background-color: ${({ theme }) => theme.color.firstColorLight};
+  }
+
+  .swiper-pagination-bullet-active {
+    background-color: ${({ theme }) => theme.color.firstColorLighten};
+  }
+`;
+
+export const TestimonialContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 375px;
+  margin: auto;
+`;
+
+export const TestimonialImage = styled.img`
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+  margin-bottom: ${({ theme }) => theme.margin.mb_1};
+`;
+
+export const TestimonialName = styled.h3`
+  font-size: ${({ theme }) => theme.font.h3FontSize};
+`;
+
+export const TestimonialClient = styled.span`
+  font-size: ${({ theme }) => theme.font.smallFontSize};
+  color: ${({ theme }) => theme.color.firstColorLight};
+`;
+
+export const TestimonialDescription = styled.p`
+  margin: ${({ theme }) => theme.margin.mb_2} 0 ${({ theme }) => theme.margin.mb_6};
+`;
