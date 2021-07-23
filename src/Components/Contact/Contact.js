c9a9865dd0942888f2f7e04eb788c042af90d9c5
Omar <omaar_5@hotmail.com>
2021-07-23 15:18:01 +0200

    feat: Contact part-1

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,11 +1,87 @@
+import { Button, Container, Section, SectionSubtitle, SectionTitle } from "../../GlobalStyle";
 import {
     ContactContainer,
+    ContactContent,
+    ContactBox,
+    ContactIcon,
+    ContactTitle,
+    ContactText,
+    ContactSocial,
+    ContactForm,
+    ContactInputs,
+    Input,
+    TextArea,
 } from "./styledContact";
 
 export const Contact = () => {
     return (
-        <ContactContainer>
-            Contact Component
-        </ContactContainer>
+        <Section id="contact">
+            <Container>
+                <SectionSubtitle>For projects</SectionSubtitle>
+                <SectionTitle>Contact Me</SectionTitle>
+
+                <ContactContainer>
+                    <ContactContent>
+                        <ContactBox>
+                            <ContactIcon>
+                                <i className="bx bx-home"></i>
+                            </ContactIcon>
+                            <ContactTitle>Location</ContactTitle>
+                            <ContactText>#123 Lima-peru</ContactText>
+                        </ContactBox>
+
+                        <ContactBox>
+                            <ContactIcon>
+                                <i className="bx bx-envelope"></i>
+                            </ContactIcon>
+                            <ContactTitle>Email</ContactTitle>
+                            <ContactText>email@gmail.com</ContactText>
+                        </ContactBox>
+
+                        <ContactBox>
+                            <ContactIcon>
+                                <i className="bx bx-phone"></i>
+                            </ContactIcon>
+                            <ContactTitle>Phone</ContactTitle>
+                            <ContactText>+0-999-888-777</ContactText>
+                        </ContactBox>
+
+                        <ContactBox>
+                            <ContactIcon>
+                                <i className="bx bx-chat"></i>
+                            </ContactIcon>
+                            <ContactTitle>Chat</ContactTitle>
+                            <div>
+                                <ContactSocial href="#">
+                                    <i className="bx bxl-whatsapp-square"></i>
+                                </ContactSocial>
+                                <ContactSocial href="#">
+                                    <i className="bx bxl-telegram"></i>
+                                </ContactSocial>
+                                <ContactSocial href="#">
+                                    <i className="bx bxl-messenger"></i>
+                                </ContactSocial>
+                            </div>
+                        </ContactBox>
+                    </ContactContent>
+
+                    <ContactForm>
+                        <ContactInputs>
+                            <Input type="text" placeholder="Name" />
+                            <Input type="email" placeholder="Email" />
+                        </ContactInputs>
+
+                        <ContactInputs>
+                            <Input type="text" placeholder="Project" />
+                            <Input type="text" placeholder="Phone Number" />
+                        </ContactInputs>
+
+                        <TextArea cols="0" rows="7" placeholder="Message"></TextArea>
+
+                        <Button contact type="submit">Send Message</Button>
+                    </ContactForm>
+                </ContactContainer>
+            </Container>
+        </Section>
     );
 };
--- src/Components/Contact/styledContact.js
+++ src/Components/Contact/styledContact.js
@@ -1,3 +1,76 @@
 import styled from 'styled-components/macro';
 
-export const ContactContainer = styled.div``;
+export const ContactContainer = styled.div`
+  display: grid;
+  row-gap: 2.5rem;
+`;
+
+export const ContactContent = styled.div`
+  display: grid;
+  gap: 1.5rem;
+  grid-template-columns: repeat(auto-fit, minmax(220px, 1fr));
+`;
+
+export const ContactBox = styled.div`
+  background-color: #fff;
+  border-radius: 0.5rem;
+  padding: 1.5rem;
+  text-align: center;
+  box-shadow: 0 4px 6px rgba(174, 190, 205, 0.3);
+  transition: 0.5s;
+
+  :hover {
+    box-shadow: 0 6px 8px rgba(174, 190, 205, 0.4);
+  }
+`;
+
+export const ContactIcon = styled.div`
+  color: ${({ theme }) => theme.color.firstColor};
+  font-size: 2rem;
+`;
+
+export const ContactTitle = styled.h3`
+  color: ${({ theme }) => theme.color.firstColor};
+  font-size: ${({ theme }) => theme.font.h3FontSize};
+  margin: ${({ theme }) => theme.margin.mb_1} 0;
+`;
+
+export const ContactText = styled.span``;
+
+export const ContactSocial = styled.a`
+  color: ${({ theme }) => theme.color.firstColorLight};
+  font-size: 1.25rem;
+  margin: 0 ${({ theme }) => theme.margin.mb_1};
+`;
+
+export const ContactForm = styled.form``;
+
+export const ContactInputs = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  column-gap: 1rem;
+`;
+
+export const Input = styled.input`
+  outline: 0;
+  font-family: ${({ theme }) => theme.font.bodyFont};
+  font-size: ${({ theme }) => theme.font.normalFontSize};
+  width: 100%;
+  padding: 1rem;
+  border: 2px solid ${({ theme }) => theme.color.firstColorLight};
+  color: ${({ theme }) => theme.color.firstColor};
+  border-radius: 0.5rem;
+  margin-bottom: ${({ theme }) => theme.margin.mb_1};
+`;
+
+export const TextArea = styled.textarea`
+  outline: 0;
+  font-family: ${({ theme }) => theme.font.bodyFont};
+  font-size: ${({ theme }) => theme.font.normalFontSize};
+  width: 100%;
+  padding: 1rem;
+  border: 2px solid ${({ theme }) => theme.color.firstColorLight};
+  color: ${({ theme }) => theme.color.firstColor};
+  border-radius: 0.5rem;
+  margin-bottom: ${({ theme }) => theme.margin.mb_1};
+`;
