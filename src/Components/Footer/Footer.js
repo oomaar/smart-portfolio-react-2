1502f8a8abd622a74462425bbe995e551e85aa68
Omar <omaar_5@hotmail.com>
2021-07-24 15:21:21 +0200

    feat: Footer part-1

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,11 +1,37 @@
+import { Container } from "../../GlobalStyle";
 import {
     FooterTag,
+    FooterSubcontainer,
+    FooterTitle,
+    FooterDescription,
+    FooterSocial,
+    FooterSocialIcon,
+    FooterCopy,
 } from "./styledFooter";
 
-export const Footer = () => {
+export const Footer = ({ data }) => {
     return (
         <FooterTag>
-            Footer Component
+            <Container>
+                <FooterSubcontainer>
+                    <FooterTitle>{data.title}</FooterTitle>
+                    <FooterDescription>{data.description}</FooterDescription>
+
+                    <FooterSocial>
+                        <FooterSocialIcon href="#">
+                            <i className="bx bxl-facebook"></i>
+                        </FooterSocialIcon>
+                        <FooterSocialIcon href="#">
+                            <i className="bx bxl-instagram"></i>
+                        </FooterSocialIcon>
+                        <FooterSocialIcon href="#">
+                            <i className="bx bxl-github"></i>
+                        </FooterSocialIcon>
+                    </FooterSocial>
+
+                    <FooterCopy>&#169; {data.copy}</FooterCopy>
+                </FooterSubcontainer>
+            </Container>
         </FooterTag>
     );
 };
--- src/Components/Footer/styledFooter.js
+++ src/Components/Footer/styledFooter.js
@@ -1,3 +1,41 @@
 import styled from 'styled-components/macro';
 
-export const FooterTag = styled.footer``;
+export const FooterTag = styled.footer`
+  background-color: ${({ theme }) => theme.color.firstColorDark};
+  color: ${({ theme }) => theme.color.firstColorLighten};
+  text-align: center;
+`;
+
+export const FooterSubcontainer = styled.div`
+  padding: 3rem 0;
+`;
+
+export const FooterTitle = styled.h1`
+  font-size: ${({ theme }) => theme.font.h1FontSize};
+  margin-bottom: ${({ theme }) => theme.margin.mb_1};
+  font-weight: ${({ theme }) => theme.font.fontWeight.semiBold};
+`;
+
+export const FooterDescription = styled.p`
+  margin-bottom: ${({ theme }) => theme.margin.mb_6};
+`;
+
+export const FooterSocial = styled.div`
+  margin-bottom: ${({ theme }) => theme.margin.mb_2};
+`;
+
+export const FooterSocialIcon = styled.a`
+  font-size: 1.4rem;
+  color: ${({ theme }) => theme.color.firstColorLighten};
+  margin-bottom: 0 ${({ theme }) => theme.margin.mb_1};
+  transition: 0.3s;
+
+  :hover {
+    color: ${({ theme }) => theme.color.firstColorLight};
+  }
+`;
+
+export const FooterCopy = styled.p`
+  font-size: ${({ theme }) => theme.font.smallerFontSize};
+  color: ${({ theme }) => theme.color.firstColorLight};
+`;
