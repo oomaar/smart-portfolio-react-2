ec01f12097c880c3cc974be288d0aab8d7657a7c
Omar <omaar_5@hotmail.com>
2021-07-24 17:02:00 +0200

    feat: Project part-2

--- src/Components/Project/styledProject.js
+++ src/Components/Project/styledProject.js
@@ -6,20 +6,44 @@ export const ProjectContainer = styled.div`
   color: ${({ theme }) => theme.color.firstColorLighten};
   text-align: center;
   border-radius: 0.5rem;
+
+  @media screen and (min-width: 768px) {
+    padding: 4.5rem 0;
+  }
 `;
 
-export const ProjectData = styled.div``;
+export const ProjectData = styled.div`
+  @media screen and (min-width: 768px) {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+  }
+`;
 
 export const ProjectIcon = styled.div`
   margin-bottom: ${({ theme }) => theme.margin.mb_1};
   font-size: 3.5rem;
+
+  @media screen and (min-width: 768px) {
+    margin: 0;
+    font-size: 6rem;
+  }
 `;
 
 export const ProjectTitle = styled.h2`
   margin-bottom: ${({ theme }) => theme.margin.mb_1};
   font-size: 1.5rem;
+
+  @media screen and (min-width: 768px) {
+    margin: 0;
+    font-size: 2.5rem;
+  }
 `;
 
 export const ProjectDescription = styled.p`
   margin-bottom: ${({ theme }) => theme.margin.mb_4};
+
+  @media screen and (min-width: 768px) {
+    margin: 0;
+  }
 `;
